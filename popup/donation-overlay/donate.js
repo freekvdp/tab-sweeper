fecba025af0ac8ab386cbd2452cfb339c7028ab5
freek.vanderploeg <freek.vanderploeg@tesorion.nl>
2021-02-08 21:06:13 +0100

    refactor donation page

--- popup/donation-overlay/donate.js
+++ popup/donation-overlay/donate.js
@@ -2,5 +2,5 @@ document.getElementById('donation-qr').addEventListener("click", function() {
     browser.tabs.create({
         url: 'https://bunq.me/tabsweeper',
         active: true
-    }).then(_ => console.log('good'), _ => console.log('bad'))
+    })
 });
